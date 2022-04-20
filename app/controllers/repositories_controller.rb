1274c0609c071eff10129335ba47f45ab1da86fe
Sami Dghim <samidghim@gmail.com>
2022-04-20 07:23:36 +0100

    FEATURE-add-pagination

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,31 +1,16 @@
 class RepositoriesController < ApplicationController
   include RepositoriesHelper
-
   def search
     @repositories = []
-    @query = "d"
-    @sort_by = ""
-    @order = ""
-    @route = if @query.nil?
-      "repositories/" 
+    @query = ""
+
+    @query = params[:query] if params[:query]
+    @repositories = if @query.present?
+      RepositoriesHelper.fetch_repositories( params[:query], params[:page].to_i)
     else
-      "search/repositories"
+      RepositoriesHelper.get_all_repositories
     end
 
-    response = RepositoriesHelper.fetch_repository( @route, @query, @sort_by, @order )
-
-    json_response = JSON.parse(response.body)
-    @total_count = response.body["total_count"]
-
-    @repositories = json_response[ "items" ].map{|repo| Repository.new(
-      id: repo["id"],
-      full_name: repo["full_name"],
-      description: repo["description"],
-      stargazers_count: repo["stargazers_count"],
-      watchers_count: repo["watchers_count"],
-      forks_count: repo["forks_count"],
-      html_url: repo["html_url"],
-      avatar_url: repo["owner"]["avatar_url"]
-    )}
+    @paginatable_array = Kaminari.paginate_array(@repositories).page(params[:page]).per(30)
   end
 end
--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,11 +1,29 @@
 module RepositoriesHelper
-  def self.fetch_repository(route, query, sort_by, order )
-    uri = URI.parse($GITHUB_BASE_URL + "#{route}?q=#{query}&sortBy=#{sort_by}&order=#{order}"  )
+  
+  def self.get_all_repositories
+    uri = URI.parse($GITHUB_BASE_URL +"repositories" )
     request = Net::HTTP::Get.new(uri)
     req_options = { use_ssl: uri.scheme == "https"}
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-    response
+    json_response = JSON.parse(response.body)
+
+    return [] if json_response.empty?
+    json_response.map { |repo| Repository.jsonToModel(repo) }.to_a
+  end
+  
+  def self.fetch_repositories(query, page )
+    uri = URI.parse( $GITHUB_BASE_URL + "search/repositories?q=#{query}&page=#{page}" )
+
+    request = Net::HTTP::Get.new(uri)
+    req_options = { use_ssl: uri.scheme == "https"}
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    json_response = JSON.parse(response.body)
+    return [] if json_response.empty?
+    json_response[ "items" ].map { |repo| Repository.jsonToModel(repo) }.to_a
   end
 end
